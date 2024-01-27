879647a0acb5fb736df47575f5775e8f0e48eada
2INHEE <dlsgml3329@gmail.com>
2024-01-27 15:45:27 +0900

    Add Mention of biggestNumber

--- overallSkills/untitled/src/tests/biggestNumber.java
+++ overallSkills/untitled/src/tests/biggestNumber.java
@@ -10,7 +10,7 @@ public class biggestNumber {
         for(int num : numbers) {
             nums[idx++] = String.valueOf(num);
         }
-
+        // Comparator Override 를 통해 nums 배열 재정렬
         Arrays.sort(nums, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
@@ -20,7 +20,7 @@ public class biggestNumber {
                 return s1.compareTo(s2);
             }
         });
-
+        // 맨 앞 원소가 0이면 계산 x
         if (nums[0].equals("0")) {
             return "0";
         }
