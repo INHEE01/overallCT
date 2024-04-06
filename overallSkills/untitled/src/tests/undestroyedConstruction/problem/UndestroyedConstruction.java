c3b73e99da55e19af78cf443f65ef6bc7fd5cd1d
2INHEE <dlsgml3329@gmail.com>
2024-04-06 22:08:51 +0900

    Add solution of UndestroyedConstruction (Right Solution)

--- overallSkills/untitled/src/tests/undestroyedConstruction/problem/UndestroyedConstruction.java
+++ overallSkills/untitled/src/tests/undestroyedConstruction/problem/UndestroyedConstruction.java
@@ -2,29 +2,34 @@ package tests.undestroyedConstruction.problem;
 
 public class UndestroyedConstruction {
     public int solution(int[][] board, int[][] skill) {
-        for (int[] cast : skill) {
-            int type = cast[0];
-            int r1 = cast[1];
-            int c1 = cast[2];
-            int r2 = cast[3];
-            int c2 = cast[4];
-            int degree = cast[5];
+        int n = board.length;
+        int m = board[0].length;
+        int[][] map = new int[n + 2][m + 2];
 
+        for (int[] sk : skill) {
+            int type = sk[0], r1 = sk[1], c1 = sk[2], r2 = sk[3], c2 = sk[4], degree = sk[5];
             if (type == 1) {
                 degree *= -1;
             }
 
-            for (int i = r1; i <= r2; i++) {
-                for (int j = c1; j <= c2; j++) {
-                    board[i][j] += degree;
-                }
+            map[r1 + 1][c1 + 1] += degree; // 왼쪽 상단
+            map[r1 + 1][c2 + 2] -= degree; // 오른쪽 상단 경계 뒤
+            map[r2 + 2][c1 + 1] -= degree; // 왼쪽 하단 경계 아래
+            map[r2 + 2][c2 + 2] += degree; // 오른쪽 하단 경계 뒤
+        }
+        // 변화계산
+        for (int i = 1; i <= n + 1; i++) {
+            for (int j = 1; j <= m + 1; j++) {
+                map[i][j] += map[i - 1][j] + map[i][j - 1] - map[i - 1][j - 1];
             }
         }
-
         int answer = 0;
-        for (int[] row : board) {
-            for (int col : row) {
-                if (col > 0) {
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                board[i - 1][j - 1] += map[i][j];
+                // 변화가 적용된 board의 값이 양수라면 answer 증가
+                if (board[i - 1][j - 1] > 0) {
                     answer++;
                 }
             }
