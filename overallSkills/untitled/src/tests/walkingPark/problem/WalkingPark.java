0f39e06f78773e60a5ba2b0ba90013bb67e15785
2INHEE <dlsgml3329@gmail.com>
2024-05-26 22:11:08 +0900

    Add Solution of WalkingPark (still not satisfying all cases)

--- overallSkills/untitled/src/tests/walkingPark/problem/WalkingPark.java
+++ overallSkills/untitled/src/tests/walkingPark/problem/WalkingPark.java
@@ -3,64 +3,52 @@ package tests.walkingPark.problem;
 public class WalkingPark {
     public int[] solution(String[] park, String[] routes) {
         int[] answer = new int[2];
-        // 시작 위치와 장애물 x좌표와 y좌표 기준으로 체크위치 체크
-        int startX=0;
-        int startY=0;
-        int maxX = park[0].length()-1;
-        int maxY = park.length-1;
-        // 가로 동선, 세로 동선
-        String[] parkX = park;
-        String[] parkY = new String[maxX + 1];
+        int startX = 0;
+        int startY = 0;
+        int maxX = park[0].length() - 1;
+        int maxY = park.length - 1;
 
-        for(int i=0; i<=maxY; i++) {
-            String line = park[i];
-            for(int j=0; j<=maxX; j++) {
-                char point = line.charAt(j);
-                if(point == 'S') {
+        for (int i = 0; i <= maxY; i++) {
+            for (int j = 0; j <= maxX; j++) {
+                if (park[i].charAt(j) == 'S') {
                     startX = j;
                     startY = i;
                 }
-
-                if(parkY[j] == null) parkY[j] = "";
-                parkY[j] += point;
             }
         }
-        // 산책시작
+
         int moveX = startX;
         int moveY = startY;
-        for(String route : routes) {
-            String op = route.split(" ")[0];
-            int n = Integer.valueOf(route.split(" ")[1]);
-            // 동쪽으로 이동하는 경우
-            if(op.equals("E")) {
-                int startIdx = moveX;
-                int endIdx = moveX + n;
-                if(endIdx > maxX) endIdx = maxX;
+        for (String route : routes) {
+            String[] parts = route.split(" ");
+            String op = parts[0];
+            int n = Integer.parseInt(parts[1]);
 
-                int blockIdx = parkX[moveY].substring(startIdx, (endIdx + 1)).indexOf("X");
-                if(blockIdx < 0 && moveX + n <= maxX) moveX = endIdx;
-            } else if(op.equals("W")){ // 서쪽으로 이동하는 경우
-                int startIdx = 0;
-                int endIdx = moveX;
-                if(moveX > n) startIdx = moveX - n;
-
-                int blockIdx = parkX[moveY].substring(startIdx, (endIdx + 1)).lastIndexOf("X");
-                if(blockIdx < 0 && moveX - n >= 0) moveX = startIdx;
-            } else if(op.equals("S")) { // 남쪽으로 이동하는 경우
-                int startIdx = 0;
-                int endIdx = moveY;
-                if(moveY > n) startIdx = moveY - n;
-
-                int blockIdx = parkY[moveX].substring(startIdx, (endIdx+1)).indexOf("X");
-                if(blockIdx < 0 && moveY + n <= maxY) moveY = endIdx;
-            } else if(op.equals("N")) {
-                int startIdx = 0;
-                int endIdx = moveY;
-                if(moveY > n) startIdx = moveY - n;
-
-                int blockIdx = parkY[moveX].substring(startIdx, (endIdx + 1)).lastIndexOf("X");
-
-                if(blockIdx < 0 && moveY - n >= 0) moveY = startIdx;
+            switch (op) {
+                case "E":
+                    for (int i = 1; i <= n; i++) {
+                        if (moveX + 1 > maxX || park[moveY].charAt(moveX + 1) == 'X') break;
+                        moveX++;
+                    }
+                    break;
+                case "W":
+                    for (int i = 1; i <= n; i++) {
+                        if (moveX - 1 < 0 || park[moveY].charAt(moveX - 1) == 'X') break;
+                        moveX--;
+                    }
+                    break;
+                case "S":
+                    for (int i = 1; i <= n; i++) {
+                        if (moveY + 1 > maxY || park[moveY + 1].charAt(moveX) == 'X') break;
+                        moveY++;
+                    }
+                    break;
+                case "N":
+                    for (int i = 1; i <= n; i++) {
+                        if (moveY - 1 < 0 || park[moveY - 1].charAt(moveX) == 'X') break;
+                        moveY--;
+                    }
+                    break;
             }
         }
 
@@ -68,4 +56,59 @@ public class WalkingPark {
         answer[1] = moveX;
         return answer;
     }
+//    public int[] solution(String[] park, String[] routes) {
+//        int[] answer = new int[2];
+//        int startX = 0;
+//        int startY = 0;
+//        int maxX = park[0].length() - 1;
+//        int maxY = park.length - 1;
+//
+//        // 시작 위치 찾기
+//        for (int i = 0; i <= maxY; i++) {
+//            for (int j = 0; j <= maxX; j++) {
+//                if (park[i].charAt(j) == 'S') {
+//                    startX = j;
+//                    startY = i;
+//                }
+//            }
+//        }
+//
+//        int moveX = startX;
+//        int moveY = startY;
+//        for (String route : routes) {
+//            String op = route.split(" ")[0];
+//            int n = Integer.valueOf(route.split(" ")[1]);
+//
+//            switch (op) {
+//                case "E": // 동쪽으로 이동
+//                    for (int i = 1; i <= n; i++) {
+//                        if (moveX + i > maxX || park[moveY].charAt(moveX + i) == 'X') break;
+//                        moveX += i;
+//                    }
+//                    break;
+//                case "W": // 서쪽으로 이동
+//                    for (int i = 1; i <= n; i++) {
+//                        if (moveX - i < 0 || park[moveY].charAt(moveX - i) == 'X') break;
+//                        moveX -= i;
+//                    }
+//                    break;
+//                case "S": // 남쪽으로 이동
+//                    for (int i = 1; i <= n; i++) {
+//                        if (moveY + i > maxY || park[moveY + i].charAt(moveX) == 'X') break;
+//                        moveY += i;
+//                    }
+//                    break;
+//                case "N": // 북쪽으로 이동
+//                    for (int i = 1; i <= n; i++) {
+//                        if (moveY - i < 0 || park[moveY - i].charAt(moveX) == 'X') break;
+//                        moveY -= i;
+//                    }
+//                    break;
+//            }
+//        }
+//
+//        answer[0] = moveY;
+//        answer[1] = moveX;
+//        return answer;
+//    }
 }
