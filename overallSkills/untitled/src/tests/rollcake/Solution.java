c83300e87c5f274f6c64e6cc97051078cb2bcff6
2INHEE <dlsgml3329@gmail.com>
2024-07-01 17:40:05 +0900

    Update solution of Rollcake

--- overallSkills/untitled/src/tests/rollcake/Solution.java
+++ overallSkills/untitled/src/tests/rollcake/Solution.java
@@ -4,27 +4,25 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] topping) {
-        int answer=0;
-        HashSet<Integer> m1 = new HashSet<>();
-        HashSet<Integer> m2 = new HashSet<>();
+        int answer = 0;
+        Map<Integer, Integer> m1 = new HashMap<>();
+        Map<Integer, Integer> m2 = new HashMap<>();
 
-        int idx=1;
-        for(int i=0; i<topping.length-1; i++) {
-            for(int j=0; j<topping.length; j++) {
-                if(j < idx) {
-                    m1.add(topping[j]);
-                } else {
-                    m2.add(topping[j]);
-                }
+        for(int elem : topping) {
+            m2.put(elem, m2.getOrDefault(elem, 0)+1);
+        }
+        // 1=4, 2=2, 3=1, 4=1
+        for(int elem : topping) {
+            m1.put(elem, m1.getOrDefault(elem, 0)+1);
+            if(m2.get(elem)-1 == 0) {
+                m2.remove(elem);
+            } else {
+                m2.put(elem, m2.get(elem)-1);
             }
 
-            int c1 = m1.size();
-            int c2 = m2.size();
-            if(c1 == c2) {
+            if(m1.size() == m2.size()) {
                 answer++;
             }
-
-            idx++;
         }
 
         return answer;
