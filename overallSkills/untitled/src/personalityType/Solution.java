0fb1e49829de254be55ba1af8d8694606e4f8443
2INHEE <dlsgml3329@gmail.com>
2024-07-12 17:08:58 +0900

    Optimizing solution of PersonalityType

--- overallSkills/untitled/src/personalityType/Solution.java
+++ overallSkills/untitled/src/personalityType/Solution.java
@@ -4,40 +4,26 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] survey, int[] choices) {
-        // MBTI
-        String answer = "";
-        Map<String, Integer> surveyMap = new HashMap<>();
-        String[] category = {"R", "T", "C", "F", "J", "M", "A", "N"};
-        // List<String> category = Arrays.asList(new String[]{"R", "T", "C", "F", "J", "M", "A", "N"});
-        // MBTI 별 점수 hashmap으로 점수 구현
-        for(String s : category) {
-            surveyMap.put(s, 0);
-        }
-        // 문항 검사 후 점수 부여
+        // Map 구현
+        Map<Character, Integer> map = new HashMap<>();
         for(int i=0; i<survey.length; i++) {
-            String[] tmp = survey[i].split("");
+            int value = choices[i];
 
-            if(choices[i] == 4) {
-                continue;
-            }
-
-            if(choices[i] >= 5) {
-                surveyMap.put(tmp[1], surveyMap.get(tmp[1])+choices[i]-4);
-            } else {
-                surveyMap.put(tmp[0], surveyMap.get(tmp[0])+4-choices[i]);
+            if(value > 0 && value < 4) {
+                char ch = survey[i].charAt(0);
+                map.put(ch, map.getOrDefault(ch, 0)+4-value);
+            } else if (value > 4) {
+                char ch = survey[i].charAt(1);
+                map.put(ch, map.getOrDefault(ch, 0)+value-4);
             }
         }
-        // 점수 체크 후 성격 유형 출력
+
         StringBuilder sb = new StringBuilder();
-        for(int i=0; i<8; i+=2) {
-            if(surveyMap.get(category[i]) >= surveyMap.get(category[i+1])) {
-                sb.append(category[i]);
-            }  else {
-                sb.append(category[i+1]);
-            }
-        }
-        answer = sb.toString();
+        sb.append(map.getOrDefault('R', 0) >= map.getOrDefault('T', 0) ? 'R' : 'T');
+        sb.append(map.getOrDefault('C', 0) >= map.getOrDefault('F', 0) ? 'C' : 'F');
+        sb.append(map.getOrDefault('J', 0) >= map.getOrDefault('M', 0) ? 'J' : 'M');
+        sb.append(map.getOrDefault('A', 0) >= map.getOrDefault('N', 0) ? 'A' : 'N');
 
-        return answer;
+        return sb.toString();
     }
 }
