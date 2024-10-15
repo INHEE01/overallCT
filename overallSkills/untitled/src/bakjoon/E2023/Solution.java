ffb9e92551bed1bd14f5ec3ca62bc45f93ecee39
INHEE01 <dlsgml3329@gmail.com>
2024-10-15 17:42:13 +0900

    Add Solution of Graph<1> (dfs Algorithm)

---
+++ overallSkills/untitled/src/bakjoon/E2023/Solution.java
@@ -0,0 +1,51 @@
+package bakjoon.E2023;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static ArrayList<Integer>[] arr;
+    public static boolean[] visited;
+    public static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        dfs(2, 1);
+        dfs(3, 1);
+        dfs(5, 1);
+        dfs(7, 1);
+
+    }
+
+    public static void dfs(int num, int depth) {
+        if (depth == N) {
+            if (isPrime(num)) {
+                System.out.println(num);
+            }
+            return;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            if (isPrime(num * 10 + i)) {
+                dfs(num * 10 + i, depth + 1);
+            }
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
